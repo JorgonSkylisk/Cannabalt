0a4844ff9c04efa9802952e2bb86f5fc1568537b
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-10-01 15:42:30 +0100

    bo

---
+++ AnimationSystem.h
@@ -0,0 +1,43 @@
+#pragma once
+//library includes
+#include <map>
+//Project Includes
+#include "Animation.h"
+
+
+class AnimationSystem
+{
+
+	AnimationSystem();
+
+	//setup
+	void SetSprite(sf::Sprite& _sprite);
+	Animation& CreateAnimation(std::string _name);
+
+
+	//controls
+	void Play();
+	void Play(std::string _newAnimation);
+	void Pause();
+	void Stop();
+
+
+	//processing
+	void Update(sf::Time _frameTime);
+
+
+
+private:
+
+	//settings
+	sf::Sprite* m_sprite;
+	std::map <std::string, Animation> m_animations;
+
+
+	//state
+	Animation* m_currentAnimation;
+
+
+
+};
+
