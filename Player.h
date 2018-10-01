b8373843c833a30751c5f89e4fcbbd49fd3fe27c
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-10-01 16:56:09 +0100

    Begun player setup

---
+++ Player.h
@@ -0,0 +1,44 @@
+#pragma once
+// **** LIBRARIES ****
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+// Project Includes
+#include "AnimationSystem.h"
+
+class Player
+{
+	
+public:
+
+	Player();
+	void Input(sf::Event _gameEvent);
+	void Update(sf::Time _frameTime);
+	void Draw(sf::RenderTarget& _target);
+
+
+
+private:
+	// Assets
+	sf::Sprite m_sprite;
+	sf::Sound m_jumpSound;
+	sf::Sound m_deathSound;
+	sf::Sound m_landSound;
+	AnimationSystem m_animation;
+
+
+	// State
+	sf::Vector2f m_Velocity;
+
+
+
+};
+
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
